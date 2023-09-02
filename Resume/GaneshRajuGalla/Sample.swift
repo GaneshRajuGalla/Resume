9710d537ef0133d74e32ae78b83e888f72f753da
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-09-02 10:57:58 +0530

    Add files via upload

---
+++ Resume/GaneshRajuGalla/Sample.swift
@@ -0,0 +1,8 @@
+//
+//  Sample.swift
+//  ActionSheet
+//
+//  Created by Ganesh on 30/08/23.
+//
+
+import Foundation
---
+++ Resume/GaneshRajuGalla/utils/renderHtml.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+
+program_exists ()
+{
+    type "$1" &> /dev/null ;
+}
+
+main()
+{
+    set -e
+    pushd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) >/dev/null
+
+    if ! program_exists pandoc; then
+        echo "Please install pandoc (https://pandoc.org/installing.html): brew install pandoc"
+        exit 1
+    fi
+
+    echo "<style>" >> tmp.css
+    cat github-markdown.css >> tmp.css
+    echo "</style>" >> tmp.css
+
+    FILENAME='Ivan Zezyulya.md'
+    cp ../readme.md "$FILENAME"
+    
+    pandoc -s -f gfm -t html5 -H tmp.css "$FILENAME" > ../index.html
+
+    rm "$FILENAME"
+    rm tmp.css
+
+    popd >/dev/null
+}
+
+main "$@"
